dbcb65a7ff5c1f2e4b5a7d12ec1edc26635d7b63
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2017-04-02 12:00:42 +0800

    2017-4-2

--- script/init.sh
+++ script/init.sh
@@ -3,8 +3,10 @@
 export PATH='/etc/storage/bin:/tmp/script:/etc/storage/script:/opt/usr/sbin:/opt/usr/bin:/opt/sbin:/opt/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin'
 export LD_LIBRARY_PATH=/lib:/opt/lib
 #set -x
-hiboyfile="https://raw.githubusercontent.com/hiboyhiboy/opt-file/master"
-hiboyscript="https://raw.githubusercontent.com/hiboyhiboy/opt-script/master"
+#hiboyfile="https://raw.githubusercontent.com/hiboyhiboy/opt-file/master"
+#hiboyscript="https://raw.githubusercontent.com/hiboyhiboy/opt-script/master"
+hiboyfile="https://bitbucket.org/hiboyhiboy/opt-file/raw/master/"
+hiboyscript="https://bitbucket.org/hiboyhiboy/opt-script/raw/master/"
 ACTION=$1
 scriptfilepath=$(cd "$(dirname "$0")"; pwd)/$(basename $0)
 #echo $scriptfilepath
