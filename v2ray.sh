bebb2c7c32f55984a739dbfe1e38471611da8a4f
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2017-09-12 19:10:20 +0800

    v2ray-v2.37

--- v2ray.sh
+++ v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-version=v2.36.3
+version=v2.37
 
 SYSTEMCTL_CMD=$(command -v systemctl)
 SERVICE_CMD=$(command -v service)
