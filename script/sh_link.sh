3da65b3453fc233421d7ec791b2c4cb5378f2cc1
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2017-10-24 19:24:08 +0800

    2017-10-24+

--- script/sh_link.sh
+++ script/sh_link.sh
@@ -81,18 +81,25 @@ if [ -z "$ssr_link" ] ; then
 	return
 fi
 mkdir -p /tmp/ss/link
-wgetcurl.sh /tmp/ss/link/1_link.txt "$ssr_link" "$ssr_link" N
-if [ ! -s /tmp/ss/link/1_link.txt ] ; then
-	rm -f /tmp/ss/link/1_link.txt
-	wget --no-check-certificate --user-agent 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.3427.400 QQBrowser/9.6.12088.400' -O /tmp/ss/link/1_link.txt "$ssr_link"
+wgetcurl.sh /tmp/ss/link/0_link.txt "$ssr_link" "$ssr_link" N
+if [ ! -s /tmp/ss/link/0_link.txt ] ; then
+	rm -f /tmp/ss/link/0_link.txt
+	wget --no-check-certificate --user-agent 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.3427.400 QQBrowser/9.6.12088.400' -O /tmp/ss/link/0_link.txt "$ssr_link"
 fi
-if [ ! -s /tmp/ss/link/1_link.txt ] ; then
+if [ ! -s /tmp/ss/link/0_link.txt ] ; then
 	logger -t "【SS】" "错误！！SSR 服务器订阅文件下载失败！请检查服务器配置"
 fi
-sed -e '/^$/d' -i /tmp/ss/link/1_link.txt
-sed -e 's/$/&==/g' -i /tmp/ss/link/1_link.txt
-sed -e "s/_/\//g" -i /tmp/ss/link/1_link.txt
-sed -e "s/\-/\+/g" -i /tmp/ss/link/1_link.txt
+sed -e '/^$/d' -i /tmp/ss/link/0_link.txt
+sed -e 's/$/&==/g' -i /tmp/ss/link/0_link.txt
+sed -e "s/_/\//g" -i /tmp/ss/link/0_link.txt
+sed -e "s/\-/\+/g" -i /tmp/ss/link/0_link.txt
+cat /tmp/ss/link/0_link.txt | grep -Eo [^A-Za-z0-9+/=]+ | tr -d "\n" > /tmp/ss/link/3_link.txt
+if [ -s /tmp/ss/link/3_link.txt ] ; then
+	logger -t "【SS】" "警告！！SSR 服务器订阅文件下载包含非 BASE64 编码字符！"
+	logger -t "【SS】" "请检查服务器配置和链接："
+	logger -t "【SS】" "$ssr_link"
+fi
+cat /tmp/ss/link/0_link.txt | grep -Eo [A-Za-z0-9+/=]+ | tr -d "\n" > /tmp/ss/link/1_link.txt
 base64 -d /tmp/ss/link/1_link.txt > /tmp/ss/link/2_link.txt
 sed -e '/^$/d' -i /tmp/ss/link/2_link.txt
 sed -e 's/$/&==/g' -i /tmp/ss/link/2_link.txt
