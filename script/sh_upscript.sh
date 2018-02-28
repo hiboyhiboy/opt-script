db63fe69fdeb37930d7586e287aafc98209c05b7
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2018-02-28 09:51:25 +0800

    2018-2-27+

--- script/sh_upscript.sh
+++ script/sh_upscript.sh
@@ -19,7 +19,7 @@ wgetcurl.sh "/tmp/scriptsh.txt" "$hiboyscript/scriptsh.txt" "$hiboyscript2/scrip
 if [ -s /tmp/scriptsh.txt ] ; then
 	source /tmp/scriptsh.txt
 	nvram set scriptt="$scriptt"
-	nvram set scripto="2018-2-26"
+	nvram set scripto="2018-2-27"
 	scriptt=`nvram get scriptt`
 	scripto=`nvram get scripto`
 fi
