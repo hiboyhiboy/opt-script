16bd31098adecca473686183deea5824e8ab7af2
rufengsuixing001 <1985151814@qq.com>
2018-05-03 12:27:43 +0800

    Revert "fix echo in one line in dnspod.sh"
    This reverts commit 13e0e80d59c8924031d74085ea0f8dd63e4905d8.

--- script/Sh41_dnspod.sh
+++ script/Sh41_dnspod.sh
@@ -232,7 +232,7 @@ arDdnsCheck() {
 	local lastIP
 	source /etc/storage/ddns_script.sh
 	hostIP=$arIpAddress
-	if [ -z $(echo "$hostIP" | grep : | grep -v "\.") ] && [ "$IPv6" = "1" ] ; then 
+	if [ -z $(echo $hostIP | grep : | grep -v "\.") ] && [ "$IPv6" = "1" ] ; then 
 		IPv6=0
 		logger -t "【DNSPod动态域名】" "错误！$hostIP 获取目前 IPv6 失败，请在脚本更换其他获取地址，保证取得IPv6地址(例如:ff03:0:0:0:0:0:0:c1)"
 		return 1
