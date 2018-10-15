6ddb34515d4fccad9a2709868f53fb564fb5959e
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2018-10-16 00:04:24 +0800

    2018-10-16

--- v2ray.sh
+++ v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-version=v3.46.4
+version=v3.47
 
 [ ! -z "$( alias | grep 'alias cp=')" ] &&  unalias cp
 [ ! -z "$( alias | grep 'alias mv=')" ] &&  unalias mv
