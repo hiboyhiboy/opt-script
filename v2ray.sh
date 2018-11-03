82d597449fee5645f49fab3de57666b308e59994
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2018-11-03 19:18:26 +0800

    v2ray-linux-mipsle-v4.0

--- v2ray.sh
+++ v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-version=v3.47
+version=v4.0
 
 [ ! -z "$( alias | grep 'alias cp=')" ] &&  unalias cp
 [ ! -z "$( alias | grep 'alias mv=')" ] &&  unalias mv
