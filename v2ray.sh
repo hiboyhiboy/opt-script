6508908ed8a2947268c1a4bd066230871f0820e5
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2019-01-12 23:36:57 +0800

    V2Ray 4.11.0

--- v2ray.sh
+++ v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-version=v4.9.0
+version=v4.11.0
 
 [ ! -z "$( alias | grep 'alias cp=')" ] &&  unalias cp
 [ ! -z "$( alias | grep 'alias mv=')" ] &&  unalias mv
