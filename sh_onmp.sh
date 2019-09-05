aedce2fd2f97fd849c0b2223cc2dab46f20adb06
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2019-09-05 20:07:00 +0800

    kodexplorer4.40

--- sh_onmp.sh
+++ sh_onmp.sh
@@ -33,7 +33,7 @@ url_h5ai="https://release.larsjung.de/h5ai/h5ai-0.29.0.zip"
 url_Lychee="https://github.com/electerious/Lychee/archive/master.zip"
 
 # (7) Kodexplorer（可道云aka芒果云在线文档管理器）
-url_Kodexplorer="http://static.kodcloud.com/update/download/kodexplorer4.36.zip"
+url_Kodexplorer="http://static.kodcloud.com/update/download/kodexplorer4.40.zip"
 
 # (8) Typecho (流畅的轻量级开源博客程序)
 url_Typecho="http://typecho.org/downloads/1.1-17.10.30-release.tar.gz"
