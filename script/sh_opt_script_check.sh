1da69a14d816a9e808f668e8eebb91105aeefd51
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2020-01-11 05:34:57 +0800

    2020年1月11日

--- script/sh_opt_script_check.sh
+++ script/sh_opt_script_check.sh
@@ -56,7 +56,10 @@ fi
 
 cmd_cpu_enable=`nvram get cmd_cpu_enable`
 if [ "$cmd_cpu_enable" != "1" ] ; then
-[ -f /tmp/top ] && { rm -f /tmp/top; } || { top -n 1 | grep " R " | grep -v "top -n 1" | grep -v "grep" > /tmp/top; }
+if [ -f /tmp/top ] ; then
+rm -f /tmp/top
+else
+top -n 1 | grep " R " | grep -v "top -n 1" | grep -v "grep" > /tmp/top
 if [ -s /tmp/top ] ; then
  #21445 21444 admin    R     1972  0.4   2 24.9 COMMAND
  #  810 30601 admin    R     1588  0.3   3  2.2 top -n 1
@@ -80,27 +83,34 @@ fi
 fi
 done < /tmp/top
 
+if [ -s /tmp/top_run ] ; then
 while read line
 do
 if [ ! -z "$line" ] ; then
 top_PID="$(echo $line | awk -F '©' '{print $1}')"
+top_CPU="$(echo $line | awk -F '©' '{print $2}')"
+top_COMMAND="$(echo $line | awk -F '§' '{print $3}')"
 if [ -z "$(grep "$top_PID " /tmp/top)" ] ; then
 sed -Ei "/^$top_PID©/d" /tmp/top_run
 break
 #continue
 fi
 top_i="$(echo $line | awk -F '§' '{print $2}')"
-top_CPU="$(echo $line | awk -F '©' '{print $2}')"
 top_2i=`expr $top_i + 1`
 if [ $top_2i -gt 50 ] ; then
 kill $top_PID
 kill -9 $top_PID
 logger -t "script_check" "检测到进程 PID【$top_PID】 $top_COMMAND"
-logger -t "script_check" "已经使用CPU $top_CPU% 大于15分钟，尝试 kill 进程防卡CPU"
+logger -t "script_check" "已经连续使用CPU $top_CPU% 大于15分钟，尝试 kill 进程防卡CPU"
+sed -Ei "/^$top_PID©/d" /tmp/top_run
 fi
 sed -e "s@^$top_PID©$top_CPU©§$top_i§@$top_PID©$top_CPU©§$top_2i§@g" -i /tmp/top_run
 fi
 done < /tmp/top_run
+fi
+else
+[ -f /tmp/top_run ] && rm -f /tmp/top_run
+fi
 fi
 fi
 
