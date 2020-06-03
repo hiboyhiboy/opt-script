855837e2a730870e2cf1280740e2ae2a4e1459f4
hiboyhiboyhiboyhiboy <hiboyhiboyhiboyhiboy@gmail.com>
2020-06-03 19:03:01 +0800

    2020年6月3日

--- v2ray.sh
+++ v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-version=v4.23.3
+version=v4.23.4
 
 [ ! -z "$( alias | grep 'alias cp=')" ] &&  unalias cp
 [ ! -z "$( alias | grep 'alias mv=')" ] &&  unalias mv
